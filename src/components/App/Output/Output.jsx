778a01cae98f6f09c8e3ca42fd0b913220f876b8
John Doe <dev@dev.com>
2023-09-11 19:07:14 +0200

    Add derived state to Output

--- src/components/App/Output/Output.jsx
+++ src/components/App/Output/Output.jsx
@@ -1,14 +1,20 @@
 import Reset from "./Reset/Reset.jsx";
 import PropTypes from "prop-types";
 
-function Output({ bill }) {
+function Output({ bill, tip }) {
   Output.propTypes = {
     bill: PropTypes.number,
+    tip: PropTypes.array,
   };
+
+  const commonTip = tip.reduce((curr, acc) => curr.tip + acc.tip) / 2;
+  const commonExtra = (bill * commonTip) / 100;
 
   return (
     <div>
-      <h1>{`You pay $${bill} ($80 + $12 tip)`}</h1>
+      <h1>{`You pay $${
+        bill + commonExtra
+      } ($${bill} + $${commonExtra} tip)`}</h1>
       <Reset />
     </div>
   );
