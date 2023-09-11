77356d00d69fd01e639ddef719de225105c959f0
John Doe <dev@dev.com>
2023-09-11 19:55:50 +0200

    Add TipCalculator to prevent App re-render all the time

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,44 +1,9 @@
-import Bill from "./Bill/Bill.jsx";
-import Satisfaction from "./Satisfaction/Satisfaction.jsx";
-import Output from "./Output/Output.jsx";
-import { useState } from "react";
+import TipCalculator from "./Tipcalculator/Tipcalculator.jsx";
 
 function App() {
-  const [bill, setBill] = useState(0);
-  const [tip, setTip] = useState([
-    { id: 0, tip: 0 },
-    { id: 1, tip: 0 },
-  ]);
-
-  function handleSetBill(currBill) {
-    setBill(currBill);
-  }
-
-  function handleSetTipById(tip, id) {
-    setTip((currTip) =>
-      currTip.map((e) => (e.id === id ? { ...e, tip: tip } : e))
-    );
-  }
-
-  function handleResetCalc() {
-    setBill(0);
-    setTip([
-      { id: 0, tip: 0 },
-      { id: 1, tip: 0 },
-    ]);
-  }
-
   return (
     <>
-      <Bill bill={bill} onSetBill={handleSetBill} />
-      {[...Array(2).keys()].map((e) => (
-        <Satisfaction key={e} tip={tip} onSetTipById={handleSetTipById} id={e}>
-          How did you like the service?
-        </Satisfaction>
-      ))}
-      {bill > 0 && (
-        <Output bill={bill} tip={tip} onResetCalc={handleResetCalc} />
-      )}
+      <TipCalculator />
     </>
   );
 }
